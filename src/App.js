523a791886aa362b44b3429d228c05e50b86edf6
simon <escohen115@gmail.com>
2021-06-08 14:09:15 -0400

    a

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="image-header"src="https://www.pandasecurity.com/en/mediacenter/src/uploads/2017/09/IMG-MC-blockchain-1920x1261.jpg"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
